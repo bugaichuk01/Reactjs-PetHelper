4bb25e5b0518269bf8f69ad34fe4aeea41f07e7c
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-10 16:32:15 +0300

    Refactor authHeader token storage

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,7 +1,7 @@
 export default function authHeader() {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.Token) {
-        return {Authorization: user.Token};
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (token) {
+        return {Authorization: token};
     } else {
         return {};
     }
