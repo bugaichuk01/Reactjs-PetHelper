022dea7ad82b9d8d9734b516e41088649c74cb64
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-10 16:42:59 +0300

    Add test button 'getAllUsers'

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import API from "../../utils/API";
 
 function Home({currentUser}) {
-    API.getByUsername(currentUser.username).then(res => console.log(res.data))
-
     return (
         <div>
             <p>mail - {currentUser.email}</p>
             <p>username - {currentUser.username}</p>
             <p>status - {currentUser.status}</p>
             <p>role - {currentUser.role}</p>
+            <button onClick={() => API.getAllUsers().then(res => console.log(res.data))}>Get Users</button>
         </div>
     );
 }
