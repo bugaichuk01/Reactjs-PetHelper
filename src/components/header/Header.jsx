dc629e08b4ca9ee72068ac1692fc6914d394845b
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-11 18:42:09 +0300

    Add Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Header.module.css'
+import {Link} from "react-router-dom";
+
+function Header() {
+    return (
+        <div className={styles.header}>
+            <Link to='/'>Home</Link>
+            <Link to='/'>How it works</Link>
+            <Link to='/'>Lost/Found</Link>
+            <Link to='/report'>Report pet</Link>
+        </div>
+    );
+}
+
+export default Header;
