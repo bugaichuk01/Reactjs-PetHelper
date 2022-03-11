273f45f217ec6789c1996c0d782082be22955b0e
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-12 01:34:31 +0200

    Global redesign login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import styles from './Login.module.css'
 import {useNavigate} from "react-router-dom"
-import API from "../../utils/API";
 import UiInput from "../../components/UI/UIInput/UIInput";
 import {useDispatch} from "react-redux";
+import {login} from "../../store/actions/user";
+import usernameIcon from '../../images/user.svg'
+import passwordIcon from '../../images/password.svg'
 
 function Login() {
     const navigate = useNavigate();
@@ -18,22 +20,27 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        API.login(username, password, dispatch);
+        dispatch(login(username, password));
     }
 
     return (
         <div>
             <div className={styles.login__form_wrapper}>
-                <h1 className={styles.header__text}>Log in</h1>
-                <p className={styles.header__desc}>Welcome back! login with your data that you entered during
-                    registration</p>
+                <img className={styles.image} src={require(`../../images/logo.png`)} alt=""/>
+                <span className={styles.login__welcome}>Welcome back to PetHelper</span>
+                <p className={styles.login__desc}>Sign in to continue</p>
                 <form className={styles.login__form} onSubmit={onSubmit}>
-                    <UiInput type='text' name='username' placeholder='Username' onChange={onChange} />
-                    <UiInput type='password' name='password' placeholder='Password' onChange={onChange} />
-                    <span className={styles.recovery}>Recovery Password</span>
-                    <button className={styles.btn__login} type='submit'>Login</button>
+                    <UiInput src={usernameIcon} type='text' name='username' placeholder='Username' onChange={onChange} />
+                    <UiInput src={passwordIcon} type='password' name='password' placeholder='Password' onChange={onChange} />
+                    <button className={styles.btn__login} type='submit'>Sign in</button>
                 </form>
-                <p>Don't have an account? <span onClick={() => navigate('/register')} className={styles.register}>Register</span></p>
+                <div className={styles.login__cross}>
+                    <div className={styles.hr}/>
+                    <span className={styles.cross__text}>or</span>
+                    <div className={styles.hr}/>
+                </div>
+                <span className={styles.register}>Forgot your password?</span>
+                <p className={styles.login__desc}>Don't have an account? <span onClick={() => navigate('/register')} className={styles.register}>Register</span></p>
             </div>
         </div>
     );
