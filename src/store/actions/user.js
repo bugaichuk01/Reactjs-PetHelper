7da12790d02e31c63a7462fd2bc945f0210f3495
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-12 01:35:01 +0200

    Redux thunk work added (test)

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,15 +1,26 @@
 import {LOGIN_FAILURE, LOGIN_START, LOGIN_SUCCESS} from "../constants/actionTypes";
+import AuthService from "../../services/auth.service";
 
-export const loginStart = () => ({
-    type: LOGIN_START
-});
+export const login = (username, password) => (dispatch) => {
+    dispatch({
+       type: LOGIN_START
+    });
 
-export const loginSuccess = (user) => ({
-    type: LOGIN_SUCCESS,
-    payload: user
-});
-
-export const loginFailure = (error) => ({
-    type: LOGIN_FAILURE,
-    payload: error
-});
+    return AuthService.login(username, password).then(
+        (response) => {
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: {user: response}
+            });
+            return Promise.resolve();
+        },
+        (error) => {
+            const message = error?.response?.data?.message || error?.message || error?.toString();
+            dispatch({
+                type: LOGIN_FAILURE,
+                payload: message
+            });
+            return Promise.reject();
+        }
+    );
+};
