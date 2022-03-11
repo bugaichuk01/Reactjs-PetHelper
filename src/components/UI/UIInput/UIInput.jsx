ce65354614b36ed151fc15989447d3d2e5e31004
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-12 01:35:33 +0200

    UIInput redesign

--- src/components/UI/UIInput/UIInput.jsx
+++ src/components/UI/UIInput/UIInput.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from "./UIInput.module.css";
 
-function UiInput({type, name, placeholder, onChange}) {
+function UiInput({type, name, placeholder, onChange, src}) {
     return (
-        <input className={styles.input} type={type} name={name} placeholder={placeholder} required
-               onChange={onChange}/>
+        <div className={styles.input__wrapper}>
+            <img className={styles.icon} src={src} alt=""/>
+            <input className={styles.input} type={type} name={name} placeholder={placeholder} required
+                   onChange={onChange}/>
+        </div>
     );
 }
 
