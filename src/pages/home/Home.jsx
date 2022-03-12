62513bf160f43e983906e0a66ae08476bb575e10
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-12 19:41:32 +0300

    Add posts into homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/header/Header";
+import PostsList from "../../components/posts-list/PostsList";
 
 function Home() {
 
     return (
         <div>
-            <Header />
+            <Header/>
+            <PostsList />
         </div>
     );
 }
