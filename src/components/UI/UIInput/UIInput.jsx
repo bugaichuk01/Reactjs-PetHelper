a6c3208d82515a5de1939694acafb84af4ddfab2
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-12 16:36:45 +0300

    Refactor UIInput

--- src/components/UI/UIInput/UIInput.jsx
+++ src/components/UI/UIInput/UIInput.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from "./UIInput.module.css";
+import cn from "classnames";
 
-function UiInput({type, name, placeholder, onChange, src}) {
+function UiInput({type, name, placeholder, onChange, src, classes}) {
     return (
-        <div className={styles.input__wrapper}>
+        <div className={cn(styles.input__wrapper, classes)}>
             <img className={styles.icon} src={src} alt=""/>
             <input className={styles.input} type={type} name={name} placeholder={placeholder} required
                    onChange={onChange}/>
