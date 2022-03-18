e1cd9403d4af0235a7e2acd6eff2e38086f866df
bugaichuk01 <bugaichuk001@gmail.com>
2022-03-18 12:02:48 +0300

    Add slider to recent posts

--- src/components/posts-list/PostsList.jsx
+++ src/components/posts-list/PostsList.jsx
@@ -2,21 +2,36 @@ import React, {useEffect, useState} from 'react';
 import postService from "../../_services/post.service";
 import styles from './PostsList.module.css';
 import PostItem from "../post-item/PostItem";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 function PostsList() {
     const [posts, setPosts] = useState([]);
+    const handleDragStart = (e) => e.preventDefault();
+    const responsive = {
+        0: {
+            items: 2
+        }
+    }
 
     useEffect(() => {
         postService.getAllPosts().then(response => setPosts(response.data))
     }, [])
 
+    const items = posts.slice(0, 10).map((post) => (
+        <PostItem onDragStart={handleDragStart} post={post}/>
+    ))
+
     return (
         <ul className={styles.list}>
+            <span className={styles.list__text}>Recently Uploaded Pets</span>
             {posts && (
-                posts.slice(0, 6).map((post) => (
-                    <PostItem post={post} />
-                ))
-            )}
+                <AliceCarousel
+                    disableButtonsControls={true}
+                    responsive={responsive}
+                    mouseTracking
+                    items={items}
+                />)}
         </ul>
     );
 }
