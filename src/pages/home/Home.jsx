d20460b21e44089897585b3034f789f5b935cb83
bugaichuk01 <bugaichuk001@gmail.com>
2022-04-20 18:11:49 +0300

    Upgrade Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,34 @@
 import Header from "../../components/header/Header";
-import PostsList from "../../components/posts-list/PostsList";
+import React from 'react';
+import {Container, Typography} from "@mui/material";
+import Posts from "../../components/posts/Posts";
+import {useNavigate} from "react-router-dom";
+import useStyles from "../report/ReportStyles";
+import {Button} from "@material-ui/core";
 
 function Home() {
+    const classes = useStyles();
+    let navigate = useNavigate();
+
     return (
-        <div>
+        <React.Fragment>
             <Header/>
-            <PostsList />
-        </div>
+            <Container maxWidth={'xl'}>
+                <Typography textAlign={'center'} variant='h6'>Последние потерянные и найденные домашние животные:</Typography>
+                <Posts slice={10} items={5}>
+                    <Container sx={{textAlign: 'center', width: '300px'}}>
+                        <Button
+                            fullWidth
+                            onClick={() => navigate('/posts')}
+                            className={classes.button}
+                            variant={'contained'}
+                        >
+                            Смотреть все объявления
+                        </Button>
+                    </Container>
+                </Posts>
+            </Container>
+        </React.Fragment>
     );
 }
 
