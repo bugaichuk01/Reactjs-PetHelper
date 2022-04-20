36cbe907dacd7377497bec62764fb4a57fd03360
bugaichuk01 <bugaichuk001@gmail.com>
2022-04-20 18:14:26 +0300

    Add Posts component

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+import {ImageList} from "@mui/material";
+import PostItem from "../post-item/PostItem";
+import useScreenSize from "../../_hooks/useScreenSize";
+import postService from "../../_services/post.service";
+
+function Posts({slice, items, children}) {
+    const drawerActivate = useScreenSize();
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        postService.getAll().then(response => setPosts(response.data))
+    }, []);
+
+    return (
+        <React.Fragment>
+            <ImageList cols={drawerActivate ? 2 : items}>
+                {posts.slice(0, slice).reverse().map((post) => (
+                    <PostItem key={post.id} {...post} />
+                ))}
+            </ImageList>
+            {children}
+        </React.Fragment>
+    );
+}
+
+export default Posts;
