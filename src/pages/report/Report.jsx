88ac5319018e0eb456c86b35a179464ce291c36b
bugaichuk01 <bugaichuk001@gmail.com>
2022-04-20 18:15:03 +0300

    Refactor Report page

--- src/pages/report/Report.jsx
+++ src/pages/report/Report.jsx
@@ -1,71 +1,57 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import Header from "../../components/header/Header";
-import styles from './Report.module.css'
-import UiInput from "../../components/UI/UIInput/UIInput"
+import useStyles from './ReportStyles';
+import BasicInfo from "../../components/report/BasicInfo";
+import UploadImage from "../../components/report/UploadImage";
+import Details from "../../components/report/Details";
+import postService from "../../_services/post.service";
+
 
 function Report() {
-    const [report, setReport] = useState(true);
-    const [status, setStatus] = useState('');
+    const classes = useStyles();
+    const [postImage, setPostImage] = useState('');
     const [formData, setFormData] = useState({
-        gender: '',
         name: '',
+        status: '',
+        species: 'Кошка',
         breed: '',
-        description: ''
+        gender: '',
+        description: '',
+        eventDate:  null,
+        location:  '',
+        /*castration: '',
+        age: '',
+        location:  '',
+        city: '',
+        cash: ''*/
     });
-
-    const {gender, name, breed, description} = formData;
-
     const onChange = (event) => setFormData({...formData, [event.target.name]: event.target.value});
 
-    const onSubmit = async (e) => {
-      e.preventDefault();
-      /*await API.addReport(status, gender, name, breed, description);
-      setFormData({
-          gender: '',
-          name: '',
-          breed: '',
-          description: ''
-      });*/
+    const sendFile = useCallback((id) => {
+        const data = new FormData();
+        data.append('file', postImage);
+        data.append('id', id);
+
+        postService.postImage(data);
+    }, [postImage])
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        postService.addReport(formData).then(r => {
+            postImage && sendFile(r.data.id);
+        });
+
     }
-
-    useEffect(() => {
-        report ? setStatus('found') : setStatus('lost') ;
-    }, [report])
-
     return (
         <div>
             <Header/>
-            <div>
-                <div className={styles.select__wrapper}>
-                    <button className={report ? styles.button__select_active : styles.button__select} onClick={() => setReport(true)}>I found a pet</button>
-                    <button className={report ? styles.button__select : styles.button__select_active} onClick={() => setReport(false)}>I lost a pet</button>
-                </div>
-                {
-                    report && (
-                        <form className={styles.report__wrapper} onSubmit={onSubmit}>
-                            <UiInput value={formData.name} name='name' placeholder='Pet name (If known)' onChange={onChange} />
-                            {/*<UIButton placeholder='Last area seen' />*/}
-                            <UiInput value={formData.breed} name='breed'  placeholder='Pet breed' onChange={onChange}  />
-                            <UiInput value={formData.gender} name='gender'  placeholder='Pet gender' onChange={onChange}  />
-                            <UiInput value={formData.description} name='description'  placeholder='Description' onChange={onChange}  />
-                            <button className={styles.button__submit}>Submit</button>
-                        </form>
-                    )
-                }
-
-                {
-                    !report && (
-                        <form className={styles.report__wrapper}>
-                            <UiInput value={formData.name} name='name' placeholder='Pet name (If known)' onChange={onChange} />
-                            {/*<UIButton placeholder='Last area seen' />*/}
-                            <UiInput value={formData.breed} name='breed'  placeholder='Pet breed' onChange={onChange}  />
-                            <UiInput value={formData.gender} name='gender'  placeholder='Pet gender' onChange={onChange}  />
-                            <UiInput value={formData.description} name='description'  placeholder='Description' onChange={onChange}  />
-                            <button className={styles.button__submit}>Submit</button>
-                        </form>
-                    )
-                }
-            </div>
+            <form onSubmit={onSubmit}>
+                <BasicInfo onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
+                <UploadImage onChange={onChange} classes={classes} postImage={postImage} setPostImage={setPostImage}/>
+                <Details onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
+                <button type='submit'>submit</button>
+            </form>
         </div>
     );
 }
