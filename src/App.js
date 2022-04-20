fbc83e67a12ffefdbca5606541a06ed62b869447
bugaichuk01 <bugaichuk001@gmail.com>
2022-04-20 18:09:24 +0300

    Add new Route's

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './style.global.css';
 import Login from "./pages/auth/Login";
-import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Register from "./pages/auth/Register";
 import Home from "./pages/home/Home";
 import {useSelector} from "react-redux";
+import PostsPage from "./pages/posts-page/PostsPage";
 import Report from "./pages/report/Report";
+import Post from "./pages/post/Post";
 
 function App() {
     const {user} = useSelector(state => state.userReducer);
 
     return (
-        <Router>
             <Routes>
-                <Route path="/login" element={user ? <Navigate replace to='/'/> : <Login/>}/>
-                <Route path="/register" element={user ? <Navigate replace to='/'/> : <Register/>}/>
-                <Route path="/" element={user ? <Home/> : <Login/>}/>
-                <Route path="/report" element={user ? <Report/> : <Login/>}/>
+                <Route path="/login" element={user?.user ? <Navigate replace to='/'/> : <Login/>}/>
+                <Route path="/register" element={user?.user ? <Navigate replace to='/'/> : <Register/>}/>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/posts" element={<PostsPage/>}/>
+                <Route path="/report" element={user ? <Report/> : <Navigate replace to='/login'/>}/>
+                <Route path="/posts/:post" element={<Post/>}/>
             </Routes>
-        </Router>
     );
 }
 
