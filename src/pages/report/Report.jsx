fd6bdd5dbe7c7f0debb61b084bb353ff4dbb8c28
bugaichuk01 <bugaichuk001@gmail.com>
2022-04-27 17:12:20 +0300

    Refactor Report.jsx page

--- src/pages/report/Report.jsx
+++ src/pages/report/Report.jsx
@@ -5,27 +5,36 @@ import BasicInfo from "../../components/report/BasicInfo";
 import UploadImage from "../../components/report/UploadImage";
 import Details from "../../components/report/Details";
 import postService from "../../_services/post.service";
+import {Container} from "@mui/material";
+import {Link} from "react-router-dom";
+import SimpleAlert from "../../components/alerts/SimpleAlert";
+import useFormData from "../../_hooks/useFormData";
 
 
 function Report() {
     const classes = useStyles();
     const [postImage, setPostImage] = useState('');
-    const [formData, setFormData] = useState({
-        name: '',
+    const [status, setStatus] = useState(null);
+    const data = useFormData({
+        name: '-',
         status: '',
         species: 'Кошка',
         breed: '',
         gender: '',
-        description: '',
-        eventDate:  null,
-        location:  '',
-        /*castration: '',
-        age: '',
-        location:  '',
-        city: '',
-        cash: ''*/
-    });
-    const onChange = (event) => setFormData({...formData, [event.target.name]: event.target.value});
+        description: '-',
+        eventDate: null,
+        address: {
+            address: '',
+            x: '',
+            y: ''
+        }
+        /*
+        castration: 'Неизвестно',
+        age: '-',
+        cash: '-'
+        */
+    })
+    const {formData, setFormData, onChange} = data;
 
     const sendFile = useCallback((id) => {
         const data = new FormData();
@@ -37,22 +46,44 @@ function Report() {
 
     const onSubmit = (e) => {
         e.preventDefault();
-
         postService.addReport(formData).then(r => {
-            postImage && sendFile(r.data.id);
+            postImage &&
+            sendFile(r.data.id);
+            setStatus(r.status);
         });
-
     }
     return (
-        <div>
+        <Container maxWidth={'md'}>
             <Header/>
-            <form onSubmit={onSubmit}>
-                <BasicInfo onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
-                <UploadImage onChange={onChange} classes={classes} postImage={postImage} setPostImage={setPostImage}/>
-                <Details onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
-                <button type='submit'>submit</button>
-            </form>
-        </div>
+
+            {
+                status === 201
+                    ? (
+                        <React.Fragment>
+                            <SimpleAlert
+                                sx={{marginTop: '50px'}}
+                                severity={'success'}
+                                title={'Объявление создано!'}
+                                text={'Ваше объявление создано, вы можете просмотреть его в профиле и на главной странице сайта.'}
+                            />
+                            <div style={{textAlign: 'center', marginTop: '10px'}}>
+                                <Link to={'/'}>
+                                    Главная страница
+                                </Link>
+                            </div>
+                        </React.Fragment>
+                    )
+                    : (
+                        <form onSubmit={onSubmit}>
+                            <BasicInfo onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
+                            <UploadImage onChange={onChange} classes={classes} postImage={postImage}
+                                         setPostImage={setPostImage}/>
+                            <Details onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
+                            <button className={classes.button} type={'submit'}>Опубликовать</button>
+                        </form>
+                    )
+            }
+        </Container>
     );
 }
 
