70428455878eb2482df4cab1aedf39803d2fcdfc
bugaichuk01 <bugaichuk001@gmail.com>
2022-04-27 17:10:05 +0300

    Add PostContainer.jsx component

---
+++ src/components/post-page/post-container/PostContainer.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Avatar, Divider, Typography} from "@mui/material";
+import checkStatus from "../../../_utils/checkStatus";
+import {Box} from "@material-ui/core";
+import moment from "moment";
+
+function PostContainer({currentPost, classes}) {
+    const eventDate = moment(currentPost.eventDate).format('MMMM Do YYYY');
+    const date = moment(currentPost.date).format('MMMM Do YYYY');
+
+    return (
+        <Box className={classes.box}>
+            <img className={classes.image} src={currentPost?._links?.photoLink?.href} alt=""/>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Имя питомца:</strong> {currentPost.name}
+            </Typography>
+            <Divider/>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Пол:</strong> {currentPost.gender}
+            </Typography>
+            <Divider/>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Статус:</strong> {
+                currentPost?.species?.slice(-1) === 'а'
+                    ? currentPost.status + 'а'
+                    : currentPost.status
+            }
+            </Typography>
+            <Divider/>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Животное:</strong> {currentPost.species}
+            </Typography>
+            <Divider/>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Порода:</strong> {currentPost.breed}
+            </Typography>
+            <Divider/>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Дата {checkStatus(currentPost.status, 'пропажи', 'находки')}:</strong> {eventDate.charAt(0).toUpperCase() + eventDate.slice(1)}
+            </Typography>
+            <Divider/>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Место {checkStatus(currentPost.status, 'пропажи', 'находки')}:</strong> {currentPost?.address?.address}
+            </Typography>
+            <Divider/>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Описание:</strong> {currentPost.description}
+            </Typography>
+            <Typography className={classes.text} variant={'subtitle1'}>
+                <strong>Дата публикации:</strong> {date.charAt(0).toUpperCase() + date.slice(1)}
+            </Typography>
+        </Box>
+    );
+}
+
+export default PostContainer;
