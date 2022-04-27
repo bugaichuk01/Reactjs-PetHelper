0917012b1e423606823ce745260a98294860a947
bugaichuk01 <bugaichuk001@gmail.com>
2022-04-27 17:11:07 +0300

    Add Profile.jsx page

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Typography from "@mui/material/Typography";
+import {Box, Container, Divider} from "@mui/material";
+import Header from "../../components/header/Header";
+import ProfileCard from "../../components/profile-card/ProfileCard";
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+function Profile() {
+    return (
+        <div>
+            <Header/>
+            <Container>
+                <Typography sx={{color: 'text.primary', marginBottom: '5px'}} variant={'h4'}>Ваш аккаунт</Typography>
+                <Divider sx={{marginBottom: '25px'}}/>
+                <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+                    <ProfileCard
+                        text={'Просматривайте и управляйте вашими объявлениями'}
+                        icon={<AssignmentIcon color={'disabled'} fontSize={'large'}/>}
+                    />
+                    <ProfileCard
+                        text={'Редактируйте свой профиль и данные учетной записи'}
+                        icon={<AccountCircleIcon color={'disabled'} fontSize={'large'}/>}
+                        link={'/profile/edit'}
+                    />
+                    <ProfileCard
+                        text={'Просмотр списка сохраненных отчетов'}
+                        icon={<BookmarkIcon color={'disabled'} fontSize={'large'}/>}
+                    />
+                </Box>
+            </Container>
+        </div>
+    );
+}
+
+export default Profile;
