de8af4ca27220f146e60bcfb01a0c66dbcf9485c
bugaichuk01 <bugaichuk001@gmail.com>
2022-04-27 17:08:47 +0300

    Add map.service.js

---
+++ src/_services/map.service.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const geocode = async (geocode) => {
+    return await axios.get(`https://geocode-maps.yandex.ru/1.x/?apikey=${process.env.REACT_APP_YANDEX_GEOCODER}&format=json&geocode=${geocode}`)
+}
+
+const searchLocation = async (value) => {
+    return await axios.get(`https://search-maps.yandex.ru/v1/?apikey=${process.env.REACT_APP_YANDEX_SEARCH}&text=${value}&lang=ru_RU`)
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    geocode,
+    searchLocation
+}
