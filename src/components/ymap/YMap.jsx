ab089b205a7fe248f92b21dc99d481b89b3936e4
bugaichuk01 <bugaichuk001@gmail.com>
2022-05-08 18:12:28 +0300

    Refactor YMap.jsx component

--- src/components/ymap/YMap.jsx
+++ src/components/ymap/YMap.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Map, Placemark, YMaps} from "react-yandex-maps";
+import {Map, YMaps} from "react-yandex-maps";
 
-function YMap({setCoordinates, classes, geometry, defaultState}) {
+function YMap({setCoordinates, classes, children, defaultState}) {
     const onClickMap = (e) => setCoordinates(e.get('coords'));
 
     return (
         <YMaps>
-            <Map onClick={onClickMap}
-                 className={classes}
-                 defaultState={defaultState}>
-                <Placemark geometry={geometry}/>
+            <Map
+                onClick={onClickMap}
+                className={classes}
+                defaultState={defaultState}>
+                {children}
             </Map>
         </YMaps>
     );
