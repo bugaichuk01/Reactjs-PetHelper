7316a42fe4434b5eee74b7ca2996b82470d3fa63
bugaichuk01 <bugaichuk001@gmail.com>
2022-05-08 18:16:36 +0300

    Add GoBack.jsx component to PostsPage.jsx and delete Header.jsx

--- src/pages/posts-page/PostsPage.jsx
+++ src/pages/posts-page/PostsPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import {Container} from "@mui/material";
-import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
+import GoBack from "../../components/button/go-back/GoBack";
+
 
 function PostsPage() {
-
-
     return (
         <Container>
-            <Header/>
-            <Posts items={4} />
+            <GoBack />
+            <Posts items={3} />
         </Container>
     );
 }
