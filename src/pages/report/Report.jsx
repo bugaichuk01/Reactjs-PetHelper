97c176a79ce1190422002c6b3bc9bfc820acfcc5
bugaichuk01 <bugaichuk001@gmail.com>
2022-05-08 18:28:29 +0300

    Refactor Report.jsx page

--- src/pages/report/Report.jsx
+++ src/pages/report/Report.jsx
@@ -1,21 +1,21 @@
 import React, {useCallback, useState} from 'react';
-import Header from "../../components/header/Header";
 import useStyles from './ReportStyles';
 import BasicInfo from "../../components/report/BasicInfo";
 import UploadImage from "../../components/report/UploadImage";
 import Details from "../../components/report/Details";
 import postService from "../../_services/post.service";
-import {Container} from "@mui/material";
+import {Box, CircularProgress, Container} from "@mui/material";
 import {Link} from "react-router-dom";
 import SimpleAlert from "../../components/alerts/SimpleAlert";
 import useFormData from "../../_hooks/useFormData";
+import GoBack from "../../components/button/go-back/GoBack";
 
 
 function Report() {
     const classes = useStyles();
     const [postImage, setPostImage] = useState('');
     const [status, setStatus] = useState(null);
-    const data = useFormData({
+    const {formData, setFormData, onChange} = useFormData({
         name: '-',
         status: '',
         species: 'Кошка',
@@ -27,14 +27,12 @@ function Report() {
             address: '',
             x: '',
             y: ''
-        }
-        /*
+        },
         castration: 'Неизвестно',
-        age: '-',
-        cash: '-'
-        */
+        age: 'Неизвестно',
+        health: '',
+        award: '-'
     })
-    const {formData, setFormData, onChange} = data;
 
     const sendFile = useCallback((id) => {
         const data = new FormData();
@@ -45,44 +43,63 @@ function Report() {
     }, [postImage])
 
     const onSubmit = (e) => {
+        setStatus('loading');
         e.preventDefault();
         postService.addReport(formData).then(r => {
             postImage &&
             sendFile(r.data.id);
             setStatus(r.status);
-        });
+        })
+            .catch(err => setStatus('error'));
     }
     return (
         <Container maxWidth={'md'}>
-            <Header/>
 
-            {
-                status === 201
-                    ? (
-                        <React.Fragment>
-                            <SimpleAlert
-                                sx={{marginTop: '50px'}}
-                                severity={'success'}
-                                title={'Объявление создано!'}
-                                text={'Ваше объявление создано, вы можете просмотреть его в профиле и на главной странице сайта.'}
-                            />
-                            <div style={{textAlign: 'center', marginTop: '10px'}}>
-                                <Link to={'/'}>
-                                    Главная страница
-                                </Link>
-                            </div>
-                        </React.Fragment>
-                    )
-                    : (
-                        <form onSubmit={onSubmit}>
-                            <BasicInfo onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
-                            <UploadImage onChange={onChange} classes={classes} postImage={postImage}
-                                         setPostImage={setPostImage}/>
-                            <Details onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
-                            <button className={classes.button} type={'submit'}>Опубликовать</button>
-                        </form>
-                    )
-            }
+            <GoBack />
+            {status === 'error' && (
+                <React.Fragment>
+                    <SimpleAlert
+                        sx={{marginTop: '15px'}}
+                        severity={'error'}
+                        title={'Ошибка!'}
+                        text={'Ваше объявление не было создано, повторите попытку позже.'}
+                    />
+                    <div style={{textAlign: 'center', marginTop: '10px'}}>
+                        <Link to={'/'}>
+                            Главная страница
+                        </Link>
+                    </div>
+                </React.Fragment>
+            )}
+            {status === 'loading' && (
+                <Box sx={{textAlign: 'center'}}>
+                    <CircularProgress/>
+                </Box>
+            )}
+            {status === 201 && (
+                <React.Fragment>
+                    <SimpleAlert
+                        sx={{marginTop: '50px'}}
+                        severity={'success'}
+                        title={'Объявление создано!'}
+                        text={'Ваше объявление создано, вы можете просмотреть его в профиле и на главной странице сайта.'}
+                    />
+                    <div style={{textAlign: 'center', marginTop: '10px'}}>
+                        <Link to={'/'}>
+                            Главная страница
+                        </Link>
+                    </div>
+                </React.Fragment>
+            )}
+            {status === null && (
+                <form onSubmit={onSubmit}>
+                    <BasicInfo onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
+                    <UploadImage onChange={onChange} classes={classes} postImage={postImage}
+                                 setPostImage={setPostImage}/>
+                    <Details onChange={onChange} classes={classes} formData={formData} setFormData={setFormData}/>
+                    <button className={classes.button} type={'submit'}>Опубликовать</button>
+                </form>
+            )}
         </Container>
     );
 }
