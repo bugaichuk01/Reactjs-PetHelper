dda1c18a2e9ec0d8568d0b07f99a5c5f2983182e
bugaichuk01 <bugaichuk001@gmail.com>
2022-05-08 17:56:55 +0300

    Add comment and posts actions

--- src/store/constants/actionTypes.js
+++ src/store/constants/actionTypes.js
@@ -2,3 +2,14 @@
 export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+//comment
+export const GET_COMMENTS = 'GET_COMMENTS';
+export const POST_COMMENT = 'POST_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const FETCHING_COMMENTS = 'FETCHING_COMMENTS';
+
+//comment
+export const FETCHING_POSTS = 'FETCHING_POSTS';
+export const GET_POSTS = 'GET_POSTS';
+export const FILTER_POSTS = 'FILTER_POSTS';
