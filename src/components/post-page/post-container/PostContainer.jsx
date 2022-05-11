4a0a71b55fa384487699ea79b46e00e04d08b8ce
bugaichuk01 <bugaichuk001@gmail.com>
2022-05-11 11:02:20 +0300

    Refactor PostContainer.jsx component

--- src/components/post-page/post-container/PostContainer.jsx
+++ src/components/post-page/post-container/PostContainer.jsx
@@ -1,68 +1,84 @@
 import React from 'react';
-import {Avatar, Divider, Typography} from "@mui/material";
+import {Divider} from "@mui/material";
 import checkStatus from "../../../_utils/checkStatus";
 import {Box} from "@material-ui/core";
 import moment from "moment";
 import GoBack from "../../button/go-back/GoBack";
+import Text from "../text/Text";
+import useStyles from "../../../pages/post/PostStyles";
 
-function PostContainer({currentPost, classes}) {
+function PostContainer({currentPost}) {
+    const classes = useStyles();
     const eventDate = moment(currentPost.eventDate).format('MMMM Do YYYY');
     const date = moment(currentPost.date).format('MMMM Do YYYY');
 
     return (
         <Box className={classes.box}>
-            <GoBack />
+            <GoBack/>
             <img className={classes.image} src={currentPost?._links?.photoLink?.href} alt=""/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Имя питомца:</strong> {currentPost.name}
-            </Typography>
+            <Text
+                text={'Имя питомца:'}
+                value={currentPost.name}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Пол:</strong> {currentPost.gender}
-            </Typography>
+            <Text
+                text={'Пол:'}
+                value={currentPost?.gender}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Статус:</strong> {
-                currentPost?.species?.slice(-1) === 'а'
-                    ? currentPost.status + 'а'
-                    : currentPost.status
-            }
-            </Typography>
+            <Text
+                text={'Статус:'}
+                value={
+                    currentPost?.species?.slice(-1) === 'а'
+                        ? currentPost.status + 'а'
+                        : currentPost.status
+                }
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Животное:</strong> {currentPost.species}
-            </Typography>
+            <Text
+                text={'Животное:'}
+                value={currentPost?.species}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Порода:</strong> {currentPost.breed}
-            </Typography>
+            <Text
+                text={'Порода:'}
+                value={currentPost?.breed}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Стерелизован:</strong> {currentPost.castration}
-            </Typography>
+            <Text
+                text={'Стерелизован:'}
+                value={currentPost?.castration}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Возраст:</strong> {currentPost.age}
-            </Typography>
+            <Text
+                text={'Возраст:'}
+                value={currentPost?.age}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Состояние:</strong> {currentPost.health}
-            </Typography>
+            <Text
+                text={'Состояние:'}
+                value={currentPost?.health}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Дата {checkStatus(currentPost.status, 'пропажи', 'находки')}:</strong> {eventDate.charAt(0).toUpperCase() + eventDate.slice(1)}
-            </Typography>
+            <Text
+                text={`Дата ${checkStatus(currentPost.status, 'пропажи', 'находки')}:`}
+                value={eventDate.charAt(0).toUpperCase() + eventDate.slice(1)}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Место {checkStatus(currentPost.status, 'пропажи', 'находки')}:</strong> {currentPost?.address?.address}
-            </Typography>
+            <Text
+                text={`Место ${checkStatus(currentPost.status, 'пропажи', 'находки')}:`}
+                value={currentPost?.address?.address}
+            />
             <Divider/>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Описание:</strong> {currentPost.description}
-            </Typography>
-            <Typography className={classes.text} variant={'subtitle1'}>
-                <strong>Дата публикации:</strong> {date.charAt(0).toUpperCase() + date.slice(1)}
-            </Typography>
+            <Text
+                text={`Описание:`}
+                value={currentPost?.description}
+            />
+            <Divider/>
+            <Text
+                text={`Дата публикации:`}
+                value={date.charAt(0).toUpperCase() + date.slice(1)}
+            />
         </Box>
     );
 }
