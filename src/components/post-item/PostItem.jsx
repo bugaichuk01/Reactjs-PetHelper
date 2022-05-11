8a1d8e71b7b851fc5c9557b3f5ac6bb44658b6da
bugaichuk01 <bugaichuk001@gmail.com>
2022-05-11 18:34:10 +0300

    Add checkStatus

--- src/components/post-item/PostItem.jsx
+++ src/components/post-item/PostItem.jsx
@@ -5,15 +5,23 @@ import moment from "moment";
 import 'moment/locale/ru';
 import useStyles from "./PostItemStyles";
 import useScreenSize from "../../_hooks/useScreenSize";
+import {Box} from "@material-ui/core";
+import checkStatus from "../../_utils/checkStatus";
 
-function PostItem({id, breed, name, status, eventDate, links}) {
+function PostItem({id, breed, name, status, eventDate, links, species}) {
     const classes = useStyles();
     const drawerActivate = useScreenSize();
-
     return (
         <Link className={classes.listItem} to={`/posts/${id}`}>
             <ImageListItem key={id}>
                 <div>
+                    <Box
+                        sx={{backgroundColor: checkStatus(status, '#f2711c', '#b5cc18')}}
+                        className={classes.status}
+                    >
+                        <Typography sx={{fontWeight: 700, fontSize: '15px'}}
+                                    variant={'subtitle2'}>{status}</Typography>
+                    </Box>
                     <img
                         src={links?.[2]?.href}
                         alt={breed}
@@ -22,8 +30,9 @@ function PostItem({id, breed, name, status, eventDate, links}) {
                     />
                 </div>
                 <ImageListItemBar
-                    title={<Typography sx={{fontWeight: 600}} variant='h6'>{name}</Typography>}
-                    subtitle={<Typography
+                    title={<Typography className={classes.topText} variant='subtitle2'>{species} по кличке
+                        «{name}»</Typography>}
+                    subtitle={<Typography className={classes.bottomText}
                         variant='body2'>{status}: {moment(eventDate).format('MMMM Do YYYY')}</Typography>}
                     position="below"
                     className={classes.itemBar}
